67e4f436dba89c8e4187985a6ebfc7b1034c20f3
Darshan Parmar <darshan02parmar@gmail.com>
2026-02-05 21:48:31 +0530

    Revert "Add Hero3 minimalist hero section"

--- postcss.config.cjs
+++
@@ -1,8 +0,0 @@
-// PostCSS pipeline: Tailwind first (class expansion), Autoprefixer last (vendor prefixes).
-module.exports = {
-  plugins: {
-    // Tailwind utilities first, then vendor prefixing.
-    tailwindcss: {},
-    autoprefixer: {}
-  }
-};
